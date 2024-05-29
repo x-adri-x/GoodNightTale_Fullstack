865f25c92744ecd57360d6b9d9e42b7ab490ce98
Adrienn <adriennlenart@yahoo.com>
2024-05-29 08:28:43 +0200

    test: add new tests to illustration module

--- server/src/modules/illustration/create/illustrationCreate.spec.ts
+++ server/src/modules/illustration/create/illustrationCreate.spec.ts
@@ -24,3 +24,21 @@ it('saves an illustration to a specific tale', async () => {
   // ignores passed in id
   expect(illustrationCreated.id).not.toEqual(illustration.id)
 })
+
+it('saves an illustration with a timestamp', async () => {
+  // ARRANGE (Given)
+  const { db, tale, user } = await setupTest()
+  const { create } = createCaller(authContext({ db }, user))
+
+  // ACT (When)
+
+  const illustration = fakeIllustration({ taleId: tale.id })
+  const illustrationCreated = await create(illustration)
+
+  // ASSERT (Then)
+  expect(illustrationCreated).toEqual({
+    ...illustration,
+    id: expect.any(Number),
+    createdAt: expect.any(Date),
+  })
+})
--- server/src/modules/illustration/update/illustrationUpdate.spec.ts
+++ server/src/modules/illustration/update/illustrationUpdate.spec.ts
@@ -1,46 +1,62 @@
-import { authContext, authRepoContext } from '@tests/utils/context'
-import { fakeIllustration, fakeUser } from '@server/entities/tests/fakes'
+import { authContext } from '@tests/utils/context'
 import { createCallerFactory } from '@server/trpc'
 import setupTest from '@server/modules/tests/setup'
+import { Tale } from '@server/entities'
 import illustrationRouter from '..'
 
 const createCaller = createCallerFactory(illustrationRouter)
 
-// Example with a mocked database
 it('sets a new prompt for the illustration', async () => {
   // ARRANGE (Given)
-  const user = fakeUser()
-  const illustration = fakeIllustration({
-    taleId: 1,
-    prompt: 'Updated prompt',
+  const { db, user, tale } = await setupTest()
+  const taleWithIllustration = await db.getRepository(Tale).findOne({
+    where: { id: tale.id },
+    relations: ['illustrations'],
   })
 
-  // Example with a mocked database
-  const { update } = createCaller(
-    authRepoContext(
-      {
-        Tale: {
-          findOne: () => ({ id: 1, userId: user.id }),
-        },
-        Illustration: {
-          update: () => ({ affected: 1 }),
-          findOneByOrFail: () => ({
-            ...illustration,
-          }),
-        },
-      },
-      user
-    )
-  )
+  const illustration = {
+    taleId: tale.id,
+    id: taleWithIllustration?.illustrations[0].id!,
+    url: taleWithIllustration?.illustrations[0].url!,
+    prompt: 'Updated prompt',
+  }
+
+  const { update } = createCaller(authContext({ db }, user))
 
   // ACT (When)
   const illustrationUpdated = await update(illustration)
+  const existing = await db.getRepository(Tale).findOne({
+    where: { id: tale.id },
+    relations: ['illustrations'],
+  })
+  if (!existing) {
+    throw new Error(`Tale with id ${tale.id} not found`)
+  }
 
   // ASSERT (Then)
   expect(illustrationUpdated).toMatchObject({
     id: illustration.id,
     prompt: illustration.prompt,
   })
+})
+
+it('updates the url', async () => {
+  // it will need to check that the url is updated
+  const { db, user, tale, illustrations } = await setupTest()
+  const { update } = createCaller(authContext({ db }, user))
+
+  const illustrationToUpdate = {
+    ...illustrations[0],
+    url: 'Updated url',
+  }
+
+  const updated = await update({
+    taleId: tale.id,
+    id: illustrationToUpdate.id,
+    prompt: illustrationToUpdate.prompt,
+    url: illustrationToUpdate.url,
+  })
+  expect(updated.url).toBe('Updated url')
 })
 
 // Example with a database
