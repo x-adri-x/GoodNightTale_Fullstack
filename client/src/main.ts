cec2499f50c294b24d9a2c94c3ad6398c3f4975b
Adrienn <adriennlenart@yahoo.com>
2024-06-05 21:32:17 +0200

    feat: add vuetify

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,34 @@
-import './assets/style.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'dark',
+  },
+  components,
+  directives,
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
