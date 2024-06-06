50ed6fd518b10cb75b524fc635c00119457cf1fb
Adrienn <adriennlenart@yahoo.com>
2024-06-06 09:59:42 +0200

    feat: remove flowbite from SignupView

---
+++ client/src/components/AlertToast.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineProps, type PropType } from 'vue'
+
+type Variant = 'info' | 'warning' | 'error'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  variant: {
+    type: String as PropType<Variant>,
+    required: true,
+  },
+})
+const icon = '$'.concat(props.variant)
+</script>
+<template>
+  <v-alert :color="variant" :icon="icon" :title="title" :text="text"></v-alert>
+</template>
--- client/src/views/SignupView.vue
+++ client/src/views/SignupView.vue
@@ -1,9 +1,7 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 import { signup } from '@/stores/user'
 import { ref } from 'vue'
-import PageForm from '@/components/PageForm.vue'
-import { FwbAlert, FwbButton, FwbInput } from 'flowbite-vue'
-import AlertError from '@/components/AlertError.vue'
+import AlertToast from '@/components/AlertToast.vue'
 import useErrorMessage from '@/composables/useErrorMessage'
 import ButtonPrimary from '@/components/ButtonPrimary.vue'
 
@@ -13,84 +11,48 @@ const userForm = ref({
 })
 
 const hasSucceeded = ref(false)
-
 const [submitSignup, errorMessage] = useErrorMessage(async () => {
   await signup(userForm.value)
-
   hasSucceeded.value = true
 })
 </script>
 
 <template>
   <div class="main">
-    <div>
-      <h1>Sign up for an account</h1>
-      <v-form role="form" @submit="submitSignup">
-        <v-text-field
-          v-model="userForm.email"
-          theme="primary-darken-1"
-          label="email"
-          type="email"
-        ></v-text-field>
-        <v-text-field
-          v-model="userForm.password"
-          theme="primary-darken-1"
-          label="password"
-          type="password"
-        ></v-text-field>
-        <ButtonPrimary class="btn" type="submit" text="Sign up" />
-        <p>
-          Already a member?
-          {{ ' ' }}
-          <RouterLink :to="{ name: 'Login' }" class="bold">Log in</RouterLink>
-        </p>
-      </v-form>
-    </div>
-  </div>
-
-  <!-- <PageForm heading="Sign up for an account" formLabel="Signup" @submit="submitSignup">
-    <template #default>
-      <FwbInput label="Email" type="email" v-model="userForm.email" :required="true" />
-
-      <FwbInput
-        label="Password"
-        id="password"
-        name="password"
-        type="password"
-        autocomplete="current-password"
+    <h1>Sign up for an account</h1>
+    <v-form role="form" @submit.prevent="submitSignup">
+      <v-text-field
+        v-model="userForm.email"
+        theme="primary-darken-1"
+        label="email"
+        type="email"
+      ></v-text-field>
+      <v-text-field
         v-model="userForm.password"
-        :required="true"
+        theme="primary-darken-1"
+        label="password"
+        type="password"
+      ></v-text-field>
+      <ButtonPrimary
+        class="btn"
+        type="submit"
+        text="Sign up"
+        :isDisabled="!userForm.password || !userForm.email"
       />
-
-      <FwbAlert v-if="hasSucceeded" data-testid="successMessage" type="success">
-        You have successfully signed up! You can now log in.
-        <RouterLink
-          :to="{ name: 'Login' }"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >Go to the login page</RouterLink
-        >
-      </FwbAlert>
-      <AlertError :message="errorMessage">
-        {{ errorMessage }}
-      </AlertError>
-
-      <div class="grid">
-        <FwbButton color="default" type="submit" size="xl">Sign up</FwbButton>
-      </div>
-    </template>
-
-    <template #footer>
-      <FwbAlert class="bg-transparent text-center">
+      <p v-if="!hasSucceeded">
         Already a member?
         {{ ' ' }}
-        <RouterLink
-          :to="{ name: 'Login' }"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >Log in</RouterLink
-        >
-      </FwbAlert>
-    </template>
-  </PageForm> -->
+        <RouterLink :to="{ name: 'Login' }" class="bold">Log in</RouterLink>
+      </p>
+    </v-form>
+    <div v-if="hasSucceeded">
+      <p data-testid="successMessage">You have successfully signed up! You can now log in.</p>
+      <RouterLink :to="{ name: 'Login' }" class="bold">Go to the login page</RouterLink>
+    </div>
+    <div v-if="errorMessage">
+      <AlertToast data-testid="errorMessage" variant="error" title="Error" :text="errorMessage" />
+    </div>
+  </div>
 </template>
 <style scoped>
 .main {
@@ -104,6 +66,10 @@ const [submitSignup, errorMessage] = useErrorMessage(async () => {
 
 h1 {
   margin-bottom: 60px;
+}
+
+p {
+  margin-bottom: 20px;
 }
 
 .btn {
