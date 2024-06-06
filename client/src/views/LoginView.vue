5aab6353f9269290b16af3db66f78e5141bf1c16
Adrienn <adriennlenart@yahoo.com>
2024-06-06 21:45:37 +0200

    feat: remove flowbite from LoginView

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import { login } from '@/stores/user'
 import { ref } from 'vue'
-import PageForm from '@/components/PageForm.vue'
-import { FwbAlert, FwbButton, FwbInput } from 'flowbite-vue'
 import { useRouter } from 'vue-router'
 import useErrorMessage from '@/composables/useErrorMessage'
+import ButtonPrimary from '@/components/ButtonPrimary.vue'
+import AlertToast from '@/components/AlertToast.vue'
 
 const router = useRouter()
 
@@ -15,45 +15,72 @@ const userForm = ref({
 
 const [submitLogin, errorMessage] = useErrorMessage(async () => {
   await login(userForm.value)
-
-  router.push({ name: 'Dashboard' })
+  router.push({ name: 'Home' })
 })
 </script>
 
 <template>
-  <PageForm heading="Log in to your account" formLabel="Login" @submit="submitLogin">
-    <template #default>
-      <FwbInput label="Email" type="email" v-model="userForm.email" :required="true" />
-
-      <FwbInput
-        label="Password"
-        id="password"
-        name="password"
-        type="password"
-        autocomplete="current-password"
+  <div class="main">
+    <h1>Log in to your account</h1>
+    <v-form role="form" @submit.prevent="submitLogin">
+      <v-text-field
+        v-model="userForm.email"
+        theme="primary-darken-1"
+        label="email"
+        type="email"
+      ></v-text-field>
+      <v-text-field
         v-model="userForm.password"
-        :required="true"
+        theme="primary-darken-1"
+        label="password"
+        type="password"
+      ></v-text-field>
+      <ButtonPrimary
+        class="btn"
+        type="submit"
+        text="Log in"
+        :isDisabled="!userForm.password || !userForm.email"
       />
-
-      <FwbAlert v-if="errorMessage" data-testid="errorMessage" type="danger">
-        {{ errorMessage }}
-      </FwbAlert>
-
-      <div class="grid">
-        <FwbButton color="default" type="submit" size="xl">Log in</FwbButton>
-      </div>
-    </template>
-
-    <template #footer>
-      <FwbAlert class="bg-transparent text-center">
-        Not a member?
+      <p>
+        Not a member yet?
         {{ ' ' }}
-        <RouterLink
-          :to="{ name: 'Signup' }"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >Sign up</RouterLink
-        >
-      </FwbAlert>
-    </template>
-  </PageForm>
+        <RouterLink :to="{ name: 'Signup' }" class="bold">Sign up</RouterLink>
+      </p>
+    </v-form>
+    <div v-if="errorMessage">
+      <AlertToast data-testid="errorMessage" variant="error" title="Error" :text="errorMessage" />
+    </div>
+  </div>
 </template>
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: 2rem;
+  width: 100%;
+  justify-content: center;
+}
+
+h1 {
+  margin-bottom: 60px;
+}
+
+p {
+  margin-bottom: 20px;
+}
+
+.btn {
+  width: 100%;
+  height: 50px;
+  margin-top: auto;
+  color: blanchedalmond;
+  margin-bottom: 20px;
+}
+
+.bold {
+  color: #ed9c98;
+  font-weight: 500;
+  text-decoration: none;
+}
+</style>
