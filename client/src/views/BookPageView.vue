aef183903ed44d2eb99c85b216564d638bc6de3c
Adrienn Lenart <adriennlenart@yahoo.com>
2024-06-16 00:23:46 +0200

    refactor: remove PageWrapper component

--- client/src/views/BookPageView.vue
+++ client/src/views/BookPageView.vue
@@ -1,51 +1,66 @@
 <script setup lang="ts">
 import { trpc } from '@/trpc'
 import { ref } from 'vue'
-import PageWrapper from '@/components/PageWrapper.vue'
 import CarouselComponent from './TaleView/CarouselComponent.vue'
 import CarouselSlide from './TaleView/CarouselSlide.vue'
 import ButtonPrimary from '@/components/ButtonPrimary.vue'
 import { mdiTooltipEdit } from '@mdi/js'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
+import { handleError } from '@/utils/helpers'
+import constants from '@/constants/constants'
 
-const isFavorite = ref(false)
 const router = useRouter()
+const errorMessage = ref()
+const tale = ref()
+const route = useRoute()
+const taleId = parseInt(route.params.id as string, 10)
+
+const safeGet = handleError(trpc.tale.get.query, errorMessage)
+tale.value = await safeGet(taleId)
+
+const urls = tale.value.illustrations.map((i: { url: any }) => i.url)
+
+const pages: string[] = []
+pages.push(tale.value.title)
+const tmpBody = tale.value.body.slice()
+
+constants.illustrationIndexes.split(',').forEach((index, i) => {
+  tmpBody.splice(parseInt(index, 10), 0, urls[i]!)
+})
+pages.push(...tmpBody)
 
 const favorite = async (id: number) => {
   await trpc.tale.update.mutate({ taleId: id, isFavorite: true })
-  isFavorite.value = true
 }
 </script>
 <template>
-  <PageWrapper>
-    <template v-slot="props">
-      <div class="heading">
-        <h1>{{ props.tale.title }}</h1>
-        <v-icon
-          :icon="mdiTooltipEdit"
-          :size="30"
-          @click="router.push(`/edit/${props.tale.id}`)"
-        ></v-icon>
-      </div>
+  <div class="main">
+    <div class="heading">
+      <h1>{{ tale.title }}</h1>
+      <v-icon :icon="mdiTooltipEdit" :size="30" @click="router.push(`/edit/${tale.id}`)"></v-icon>
+    </div>
 
-      <CarouselComponent v-slot="{ currentSlide }">
-        <CarouselSlide v-for="(page, i) in props.pages" :key="i">
-          <div v-show="i === currentSlide">
-            <img v-if="i === 2 || i === 4" :src="page" width="100%" alt="alt" />
-            <div v-else :class="{ title: i === 0 }">{{ page }}</div>
-          </div>
-        </CarouselSlide>
-      </CarouselComponent>
-      <ButtonPrimary
-        v-show="!isFavorite"
-        class="btn"
-        text="Mark as Favorite"
-        @click="() => favorite(props.tale.id)"
-      />
-    </template>
-  </PageWrapper>
+    <CarouselComponent v-slot="{ currentSlide }">
+      <CarouselSlide v-for="(page, i) in pages" :key="i">
+        <div v-show="i === currentSlide">
+          <img v-if="i === 2 || i === 4" :src="page" width="100%" alt="alt" />
+          <div v-else :class="{ title: i === 0 }">{{ page }}</div>
+        </div>
+      </CarouselSlide>
+    </CarouselComponent>
+    <ButtonPrimary
+      v-show="!tale.isFavorite"
+      class="btn"
+      text="Mark as Favorite"
+      @click="() => favorite(tale.id)"
+    />
+  </div>
 </template>
 <style scoped>
+.main {
+  padding: 2rem;
+  width: 100%;
+}
 .btn {
   margin-top: 40px;
   width: 100vw;
